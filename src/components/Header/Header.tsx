a3295b396586d5cf5598cdbba6ecc8f6d0cd65ce
Oleksii Persatyi <persatyi@gmail.com>
2022-12-25 02:32:04 +0200

    update

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,15 @@
 import s from "./Header.module.scss";
 
 import Container from "components/Container";
-import Navigation from "components/Header/Navigation";
-import TextField from "@mui/material/TextField";
+import Navigation from "./Navigation";
+import SearchField from "./SearchField";
 
 const Header = () => {
   return (
     <header className={s.header}>
       <Container>
         <Navigation />
-        <TextField
-          id="standard-basic"
-          label="Search movies"
-          variant="standard"
-          size="small"
-          fullWidth={true}
-        />
+        <SearchField />
       </Container>
     </header>
   );
---
+++ src/components/Header/SearchField/SearchField.tsx
@@ -0,0 +1,9 @@
+import TextField from "@mui/material/TextField";
+
+const SearchField = () => {
+  return (
+    <TextField id="standard-basic" label="Search movies" variant="standard" />
+  );
+};
+
+export default SearchField;
