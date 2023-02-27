d2f6a52a64dae90e269e0fb202a2d5cf7473c6a1
Oleksii Persatyi <persatyi@gmail.com>
2023-02-27 16:32:51 +0200

    update

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -10,7 +10,7 @@ import { Box } from "@mui/material";
 import { useGetPopularQuery } from "services/APIService";
 import { useToggle } from "hooks";
 
-const Home = () => {
+const Home: React.FC = () => {
   const [value, toggle, setValue] = useToggle();
   const [id, setId] = useState(0);
   const [page, setPage] = useState(1);
@@ -22,58 +22,54 @@ const Home = () => {
 
   const { data, error, isLoading, isFetching } = useGetPopularQuery(page);
 
-  if (error) {
-    return <p>Something went wrong please reload the page</p>;
-  }
+  return (
+    <>
+      {error ? (
+        <p>Something went wrong please reload the page</p>
+      ) : isLoading ? (
+        <Loader />
+      ) : isFetching ? (
+        <Loader />
+      ) : data ? (
+        <>
+          <Box
+            display="grid"
+            gridTemplateColumns={{
+              xs: "repeat(1, 1fr)",
+              tablet: "repeat(2, 1fr)",
+              laptop: "repeat(3, 1fr)",
+            }}
+            gap={{ xs: 0, tablet: 2 }}
+            sx={{ justifyItems: { mobile: "center" } }}
+          >
+            {data.results.map((element: { id: number }) => (
+              <Box
+                gridColumn="span 1"
+                key={element.id}
+                onClick={() => handleModal(element.id)}
+              >
+                <MovieCard data={element} />
+              </Box>
+            ))}
+          </Box>
 
-  if (isLoading) {
-    return <Loader />;
-  }
-
-  if (isFetching) {
-    return <Loader />;
-  }
-
-  if (data) {
-    return (
-      <>
-        <Box
-          display="grid"
-          gridTemplateColumns={{
-            xs: "repeat(1, 1fr)",
-            tablet: "repeat(2, 1fr)",
-            laptop: "repeat(3, 1fr)",
-          }}
-          gap={{ xs: 0, tablet: 2 }}
-          sx={{ justifyItems: { mobile: "center" } }}
-        >
-          {data.results.map((element: { id: number }) => (
-            <Box
-              gridColumn="span 1"
-              key={element.id}
-              onClick={() => handleModal(element.id)}
-            >
-              <MovieCard data={element} />
-            </Box>
-          ))}
-        </Box>
-
-        <MoviePagination
-          page={data.page}
-          count={data.total_pages}
-          setPage={setPage}
-        />
-
-        <ModalWrapper open={value} onClose={() => setValue(false)}>
-          <MovieModal
-            onClose={() => setValue(false)}
-            id={id}
-            data={data.results}
+          <MoviePagination
+            page={data.page}
+            count={data.total_pages}
+            setPage={setPage}
           />
-        </ModalWrapper>
-      </>
-    );
-  }
+
+          <ModalWrapper open={value} onClose={() => setValue(false)}>
+            <MovieModal
+              onClose={() => setValue(false)}
+              id={id}
+              data={data.results}
+            />
+          </ModalWrapper>
+        </>
+      ) : null}
+    </>
+  );
 };
 
 export default Home;
