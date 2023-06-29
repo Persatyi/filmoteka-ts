815539b6634a111a1f4e9cfaf942a796ed9ea2aa
Олексій Персатий <persatyi@ukr.net>
2023-06-29 23:18:19 +0300

    added firebase package

---
+++ src/services/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCkvYWRlx223XaAh1UGxQYz4a5jKsPDb1I",
+  authDomain: "filmoteka-b66e8.firebaseapp.com",
+  projectId: "filmoteka-b66e8",
+  storageBucket: "filmoteka-b66e8.appspot.com",
+  messagingSenderId: "604868587191",
+  appId: "1:604868587191:web:9d3f629e0e0be984c3a732",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
