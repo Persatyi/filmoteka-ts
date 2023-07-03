c915fd0780b887c08ca3f1de9dc204734d513173
Олексій Персатий <persatyi@ukr.net>
2023-07-03 18:50:05 +0300

    added some styles

--- src/components/Header/UserDisplay/UserDisplay.tsx
+++ src/components/Header/UserDisplay/UserDisplay.tsx
@@ -1,13 +1,24 @@
 import React, { useState } from "react";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import { Box, Typography } from "@mui/material";
 import { User } from "firebase/auth";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "services/firebase";
+
+import * as s from "./UserDisplayTheme";
 
 const UserDisplay = () => {
   const [user, setUser] = useState<User | null>(null);
   onAuthStateChanged(auth, (currentUser) => setUser(currentUser));
 
-  return user && <div>{user?.displayName}</div>;
+  return (
+    <Box component="div" sx={{ display: "flex" }}>
+      <AccountCircleIcon sx={{ width: "30px", height: "30px" }} />
+      <Typography component="p" sx={s.user}>
+        {user?.displayName}
+      </Typography>
+    </Box>
+  );
 };
 
 export default UserDisplay;
---
+++ src/components/Header/UserDisplay/UserDisplayTheme.ts
@@ -0,0 +1,15 @@
+const user = {
+  display: { xs: "none", mobile: "flex" },
+  alignItems: "baseline",
+  fontFamily: "Roboto, sans-serif",
+  fontSize: 30,
+  lineHeight: 1.16,
+  fontWeight: 500,
+  letterSpacing: ".01rem",
+  color: "#ffffff",
+  padding: "0px",
+  gap: "10px",
+  minHeight: "0px",
+};
+
+export { user };
